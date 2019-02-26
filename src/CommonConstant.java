7b8ba725545e25e5222e804c13458beb226d9518
will <Awille@qq.com>
2019-02-26 15:48:23 +0800

    数据库相关参数

---
+++ src/CommonConstant.java
@@ -0,0 +1,7 @@
+public interface CommonConstant {
+    //数据库相关
+    interface Database {
+        String host = "localhost";
+        String port = "3306";
+    }
+}
---
+++ src/database/DatabaseConnect.java
@@ -0,0 +1,6 @@
+package database;
+
+public class DatabaseConnect{
+    private String connectBasicInfo = "jdbc:mysql://";
+
+}
