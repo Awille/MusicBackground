d166bca22612940b4a3c31993a8366242bf3b983
will <Awille@qq.com>
2019-03-03 14:50:07 +0800

    删除歌单 原子操作问题思考

--- src/main/java/database/crud/SongListCrud.java
+++ src/main/java/database/crud/SongListCrud.java
@@ -3,6 +3,7 @@ package database.crud;
 import bean.Song;
 import bean.songlist.SongList;
 import bean.songlist.SongListBasicInfo;
+import commonconstant.CommonConstant;
 import database.DatabaseConnectManager;
 
 import java.sql.PreparedStatement;
@@ -160,4 +161,142 @@ public class SongListCrud {
         }
         return null;
     }
+
+
+    /**
+     * 删除歌单
+     * 1、先缓存关系表中的歌单 2、先删除关系表中的歌单记录 3、删除歌单
+     * 任何一步出现错误，都要进行回退数据库
+     * @param songListId 歌单ID
+     * @return 删除歌单是否成功
+     */
+    public static boolean deleteSongList(long songListId) {
+        //先缓存关系表中的歌单
+        List<Long> songIdList = getSongIdsBySongListId(songListId);
+        //回退失败的数据
+        List<Long> failBackSongId = new ArrayList<Long>();
+        if (songIdList == null) {
+            return false;
+        }
+        if (!deleteSongListInRelation(songListId)) {
+            return false;
+        }
+        //删除歌单失败
+        if (!deleteSongList(songListId)) {
+            //用缓存回退数据
+            for (Long songId : songIdList) {
+                if (!addSongToSongList(songId, songListId)) {
+                    failBackSongId.add(songId);
+                }
+            }
+            //如果有失败的回退数据
+            if (failBackSongId.size() > 0) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean deleSongListDirectly(long songListId) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = DatabaseConnectManager.getInstance().getDatabaseConnection().prepareStatement(
+                    "DELETE FROM music.songlist WHERE song_list_id = ?");
+            preparedStatement.setLong(1, songListId);
+            if (preparedStatement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static List<Long> getSongIdsBySongListId(long songListId) {
+        List<Long> userIdList = new ArrayList<Long>();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = DatabaseConnectManager.getInstance().getDatabaseConnection().prepareStatement(
+                    "SELECT song_id FROM music.songlist_song_relation WHERE song_list_id = ?");
+            preparedStatement.setLong(1, songListId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                userIdList.add(resultSet.getLong("song_id"));
+            }
+            resultSet.close();
+            return userIdList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 在歌单 歌曲关信息表中删除
+     * @param songListId
+     * @return 删除结果
+     */
+    public static boolean deleteSongListInRelation(long songListId) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = DatabaseConnectManager.getInstance().getDatabaseConnection().prepareStatement(
+                    "DELETE FROM music.songlist_song_relation WHERE song_list_id = ?");
+            preparedStatement.setLong(1, songListId);
+            if (preparedStatement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 歌单中添加歌曲
+     * @param songId
+     * @param songListId
+     * @return 添加结果
+     */
+    public static boolean addSongToSongList(long songId, long songListId) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = DatabaseConnectManager.getInstance().getDatabaseConnection().prepareStatement(
+                    "INSERT INTO music.songlist_song_relation (song_list_id, song_id) VALUE (?, ?)");
+            preparedStatement.setLong(1, songListId);
+            preparedStatement.setLong(2, songId);
+            if (preparedStatement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
 }
