2e4c634793be333bc36a9a67b842147220710b51
will <Awille@qq.com>
2019-03-04 10:25:43 +0800

    明确处理数据回退的线程池跟处理请求的线程池

--- src/main/java/ThreadPoolService.java
+++ src/main/java/ThreadPoolService.java
@@ -1,6 +1,9 @@
 import java.util.concurrent.*;
 
 public class ThreadPoolService {
+    /**
+     * 用于处理请求的线程池
+     */
     public static ThreadPoolExecutor requestExcutor = new ThreadPoolExecutor(
             5,
             100,
@@ -13,4 +16,21 @@ public class ThreadPoolService {
                     return new Thread(r);
                 }
             });
+
+    /**
+     * 用于处理数据回退的线程池
+     */
+    public static ThreadPoolExecutor backDataExcutor = new ThreadPoolExecutor(
+            5,
+            100,
+            100,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>(),
+            new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable r) {
+                    return new Thread(r);
+                }
+            });
+
 }
